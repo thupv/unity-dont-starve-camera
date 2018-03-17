7e46c28a3da8935a1b42baeec40240438a2b0fcf
ThuPV (Thomas) <thupv.personal@gmail.com>
2018-03-17 16:26:21 +0700

    Initial commit

---
+++ Assets/LookToCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookToCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 rotationVector = transform.rotation.eulerAngles;
+		rotationVector.y = Camera.main.transform.rotation.eulerAngles.y;
+		transform.rotation = Quaternion.Euler(rotationVector);
+	}
+}
---
+++ Assets/RayCast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RayCast : MonoBehaviour {
+	private RaycastHit vision;
+	public float rayLength;
+	public float smooth = 1f;
+	private Rigidbody grabbedObject;
+	private NavMeshAgent navMeshAgent;
+	private Quaternion targetRotation;
+
+	void Start () {
+		rayLength = 4.0f;
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		navMeshAgent.angularSpeed = 0;
+		targetRotation = transform.rotation;
+	}
+	
+	void Update () {
+	    Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+	    RaycastHit hit;
+	    if (Input.GetButtonDown ("Fire1")) {
+	        if (Physics.Raycast(ray, out hit, 100)) {
+	            if (hit.collider.CompareTag("plane")) {
+					navMeshAgent.destination = hit.point;
+					navMeshAgent.Resume();
+	            }
+	        }
+	    }
+
+		if (Input.GetKeyDown (KeyCode.Q)) { 
+			targetRotation *= Quaternion.AngleAxis (90, Vector3.up);
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			targetRotation *= Quaternion.AngleAxis (-90, Vector3.up);
+		}
+
+		transform.rotation= Quaternion.Lerp (transform.rotation, targetRotation , 10 * smooth * Time.deltaTime);
+	}
+}
